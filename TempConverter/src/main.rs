85260f96681c8f0b529f73d777fdefe80e24314c
Jesus Mendoza <122926229+jesusmendoza08@users.noreply.github.com>
2025-09-17 15:27:12 +0000

    My First Program

---
+++ TempConverter/src/main.rs
@@ -0,0 +1,18 @@
+let freezingtemp:f64 = 32;
+
+fn farenheit_to_celsius(f:f64) -> f64
+{
+    
+
+}
+
+fn celsius_to_farenheit(c: f64) -> f64
+{
+
+}
+
+fn main() {
+ let temp:i32 mut = 30;
+
+    
+}
