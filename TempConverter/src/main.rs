abc1849a5e12c21d9ddcb33b4fd5574afaba72bc
Jesus Mendoza <122926229+jesusmendoza08@users.noreply.github.com>
2025-09-23 23:53:39 +0000

    assignments 1-3 working

--- TempConverter/src/main.rs
+++ TempConverter/src/main.rs
@@ -2,17 +2,29 @@ let freezingtemp:f64 = 32;
 
 fn farenheit_to_celsius(f:f64) -> f64
 {
-    
+    	
+return (f − 32) × (5/9); 
+
 
 }
 
 fn celsius_to_farenheit(c: f64) -> f64
 {
-
+    return (c × 9/5) + 32;
 }
 
 fn main() {
- let temp:i32 mut = 30;
+ let firsttemp:i32 mut = 75;
+ let mut counter = 5;
+
+ farenheit_to_celsius(firsttemp);
+
+ while counter < 5
+ {
+    firsttemp += 1;
+    farenheit_to_celsius(firsttemp);
+    counter+=1;
+ }
 
     
 }
